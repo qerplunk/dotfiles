488a64f446d2f665b97e13f6ac0e6650875c53a4
Edgar Aquino <73380268+qerplunk@users.noreply.github.com>
2025-05-31 00:49:20 -0700

    mason-null-ls requires null-ls/none-ls

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -31,6 +31,7 @@ return {
 		event = { "BufReadPre", "BufNewFile" },
 		dependencies = {
 			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
 		},
 		config = function()
 			require("mason-null-ls").setup({
