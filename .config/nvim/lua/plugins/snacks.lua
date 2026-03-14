044f526c10e129ac1a64a94e3ddbc178b4511178
Edgar Aquino <73380268+qerplunk@users.noreply.github.com>
2026-03-13 22:56:50 -0700

    Create snacks.lua

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+return {
+	"folke/snacks.nvim",
+	opts = {
+		zen = {
+			show = {
+				statusline = true,
+				tabline = true,
+			},
+			zoom = {
+				show = { statusline = true, tabline = true },
+			},
+		},
+	},
+}
